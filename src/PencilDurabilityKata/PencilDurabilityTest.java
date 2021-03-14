292e1d2644852b58dcdcecf1c2e9f0414731e297
Theo Eisenstein <theo.eisenstein@gmail.com>
2021-03-14 14:47:28 -0500

    Loading the cannon.

---
+++ src/PencilDurabilityKata/PencilDurabilityTest.java
@@ -0,0 +1,14 @@
+package PencilDurabilityKata;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PencilDurabilityTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
